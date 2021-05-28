6810af4e4c2b5e66e569928097ba8906600e8730
shefali170 <84783753+shefali170@users.noreply.github.com>
2021-05-28 14:35:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const element = React.createElement('h1',null,'Hello World!');
+
+ReactDOM.render(element,document.getElementById('root'));
